cfc44d423e40c15726486540417831e61dba795b
TV <trungvuh@gmail.com>
2017-11-15 16:15:08 -0800

    Finish thunk

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
+
 document.addEventListener("DOMContentLoaded", () => {
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Hello</h1>, rootEl);
 });
---
+++ Pokedex/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+import merge from 'lodash/merge';
+
+const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState;
+
+  switch (action.type) {
+    case RECEIVE_ALL_POKEMON:
+      return merge({}, state, action.pokemon);
+
+    default:
+      return state;
+  }
+};
+
+export default pokemonReducer;
